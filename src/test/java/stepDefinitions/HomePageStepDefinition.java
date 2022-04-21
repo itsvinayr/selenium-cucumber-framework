b3fee71c3af0601bc0064cb1552cd9b7e679f68a
Vinay Raghumanda <vinay.raghu10@gmail.com>
2022-04-21 22:32:59 +0530

    Enabling logging using log4j api

--- src/test/java/stepDefinitions/HomePageStepDefinition.java
+++ src/test/java/stepDefinitions/HomePageStepDefinition.java
@@ -2,12 +2,15 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import pageObjects.HomePage;
 import utils.TestContextSetup;
 
 public class HomePageStepDefinition {
 
     private final TestContextSetup testContextSetup;
+    private static final Logger logger = LogManager.getLogger(HomePageStepDefinition.class.getName());
 
     public HomePageStepDefinition(TestContextSetup testContextSetup){
         this.testContextSetup = testContextSetup;
@@ -16,18 +19,22 @@ public class HomePageStepDefinition {
     @Given("user lands on home page")
     public void user_lands_on_home_page() {
         testContextSetup.getTestBase().webDriverManager();
+        logger.info("Driver initialized successfully");
         testContextSetup.getPageObjectManager().getHomePage();
+        logger.info("Landed on Home Page");
     }
 
     @When("^user search for (.+)$")
     public void user_search_for(String value) {
         HomePage homePage = testContextSetup.getPageObjectManager().getHomePage();
         homePage.searchProduct(value);
+        logger.debug("Searched for product with value : "+value);
         try {
             Thread.sleep(10000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         homePage.clickOnFirstProduct();
+        logger.info("Clicked on First Product Link");
     }
 }
--- src/test/java/stepDefinitions/ProductDetailsPageStepDefinition.java
+++ src/test/java/stepDefinitions/ProductDetailsPageStepDefinition.java
@@ -1,11 +1,15 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import pageObjects.ProductDetailsPage;
 import utils.TestContextSetup;
 
 public class ProductDetailsPageStepDefinition {
     private final TestContextSetup testContextSetup;
+    private static final Logger logger = LogManager.getLogger(HomePageStepDefinition.class.getName());
 
     public ProductDetailsPageStepDefinition(TestContextSetup testContextSetup){
         this.testContextSetup = testContextSetup;
@@ -15,6 +19,12 @@ public class ProductDetailsPageStepDefinition {
     public void open_first_one_from_the_product_list_and_get_the_price() {
         testContextSetup.getGenericUtils().switchWindow();
         ProductDetailsPage productDetailsPage = testContextSetup.getPageObjectManager().getProductDetailsPage();
-        productDetailsPage.getPrice();
+        try {
+            productDetailsPage.getPrice();
+            logger.debug("Price of the product is displayed");
+        }catch (Throwable t){
+            logger.error("Couldn't retrieve product price");
+            Assert.fail("Couldn't retrieve product price");
+        }
     }
 }
